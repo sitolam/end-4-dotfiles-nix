9eaef5c61836dabe8bba4bee9d0345697d12e201
sitolam <otis.lammertyn@gmail.com>
2025-04-13 17:30:07 +0200

    cleanup: remove gnome

--- dotfiles/profiles/desktop/configuration.nix
+++ dotfiles/profiles/desktop/configuration.nix
@@ -61,7 +61,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {

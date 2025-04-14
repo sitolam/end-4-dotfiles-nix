cfb28d08da493513337074bf8eb9b81105ace63c
sitolam <otis.lammertyn@gmail.com>
2025-04-14 09:39:47 +0200

    cleanup: remove useless stuff

--- dotfiles/themes/nord.nix
+++ dotfiles/themes/nord.nix
@@ -10,24 +10,4 @@
     # Override stylix theme of btop.
     btopTheme = "nord";
 
-    # # Hyprland and ags;
-    # opacity = 1.0;
-    # rounding = 25;
-    # shadow = true;
-    # bordersPlusPlus = false;
-    # ags = {
-    #     theme = {
-    #         palette = {};
-    #         border = {
-    #             width = 1;
-    #             opacity = 96;
-    #         };
-    #     };
-    #     bar = {
-    #         curved = true;
-    #     };
-    #     widget = {
-    #         opacity = 0;
-    #     };
-    # };
 }

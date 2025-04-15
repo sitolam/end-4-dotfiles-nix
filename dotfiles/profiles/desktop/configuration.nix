30488995c73acfb6375ecf4666422994737d4684
sitolam <otis.lammertyn@gmail.com>
2025-04-15 12:18:16 +0200

    feat: add sound config

--- dotfiles/profiles/desktop/configuration.nix
+++ dotfiles/profiles/desktop/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./../../system/hardware/desktop/graphic.nix
       ./../../system/hardware/bluetooth.nix
+      ./../../system/hardware/sound.nix
     ];
 
   # Home-manager
@@ -73,20 +74,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).

73425cc685551db1868bacd600b4eff69029c50a
sitolam <otis.lammertyn@gmail.com>
2025-04-15 12:58:33 +0200

    fix

--- dotfiles/profiles/desktop/configuration.nix
+++ dotfiles/profiles/desktop/configuration.nix
@@ -74,8 +74,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 

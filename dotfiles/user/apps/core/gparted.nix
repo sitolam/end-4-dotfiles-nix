a9aef2440ee8f1d2e6b14e2fc7daaf30b2e18e52
sitolam <otis.lammertyn@gmail.com>
2025-04-18 11:32:23 +0200

    fix: gparted sudo fix

--- dotfiles/user/apps/core/gparted.nix
+++ dotfiles/user/apps/core/gparted.nix
@@ -1,19 +1,26 @@
 { config, pkgs, ...}:
 
+let
+  gparted-fix = pkgs.writeShellScriptBin "gparted-fix" ''
+    pkexec bash -c "ln -s /home/$USER/.themes /root; env GTK_THEME="adw-gtk3" XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR WAYLAND_DISPLAY=$WAYLAND_DISPLAY ${pkgs.gparted}/bin/gparted"
+    '';
+in
 
 {
   home.packages = with pkgs; [
     gparted
+    gparted-fix
   ];
 
   xdg.desktopEntries.gparted = {
     name = "GParted";
     genericName = "Partition Editor";
     comment = "Create, reorganize, and delete partitions";
-    exec = "sudo -E ${pkgs.gparted}/bin/gparted %f";
+    exec = "gparted-fix";
     icon = "gparted";
     terminal = false;
     type = "Application";
     categories = [ "GNOME" "System" "Filesystem" ];
   };
+
 }

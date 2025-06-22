3b65b66a067e4e479d91e2b46bce2240d832f6d4
sitolam <otis.lammertyn@gmail.com>
2025-06-22 23:49:29 +0200

    feat: make cleanup go faster

--- dotfiles/system/apps/core/nh.nix
+++ dotfiles/system/apps/core/nh.nix
@@ -3,7 +3,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep-since 4d --keep 2";
     flake = settings.dotfilesDir;
   };
 

0d85042ccd204975f5087334c5d6586212d030a9
sitolam <otis.lammertyn@gmail.com>
2025-06-29 18:01:00 +0200

    feat: add laptop config

---
+++ dotfiles/profiles/laptop/configuration.nix
@@ -0,0 +1,171 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, settings, lib, winapps, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./../../system/hardware/boot.nix
+      ./../../system/security/firewall.nix
+      # ./../../system/hardware/desktop/graphic.nix
+      ./../../system/hardware/bluetooth.nix
+      ./../../system/hardware/sound.nix
+      # ./../../system/virtualization/virt-manager.nix
+      # ./../../system/virtualization/winapps
+      ./../../system/apps/core
+      ./../../system/apps/media
+      # ./../../system/apps/gaming
+
+    ];
+
+  # Home-manager
+  home-manager.users.${settings.username} = {
+    imports = [
+      ./home.nix
+    ];
+  };
+  
+  home-manager.extraSpecialArgs.inputs = inputs;
+  home-manager.extraSpecialArgs.settings = settings;
+  home-manager.backupFileExtension = "bak";
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = settings.hostname; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = settings.timezone;
+
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = settings.locale;
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = settings.locale;
+    LC_IDENTIFICATION = settings.locale;
+    LC_MEASUREMENT = settings.locale;
+    LC_MONETARY = settings.locale;
+    LC_NAME = settings.locale;
+    LC_NUMERIC = settings.locale;
+    LC_PAPER = settings.locale;
+    LC_TELEPHONE = settings.locale;
+    LC_TIME = settings.timelocale;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "intl";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${settings.username} = {
+    isNormalUser = true;
+    description = settings.name;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # For BigSaltyFishes End-4 Dotfiles===============
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+  # ================================================
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    git
+    brave
+    telegram-desktop
+    
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    # noto-fonts-cjk
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+  fonts.enableDefaultPackages = false;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  programs.hyprland = {
+    enable = true;
+  };
+  services.hypridle.enable = true;
+
+}
---
+++ dotfiles/profiles/laptop/home.nix
@@ -0,0 +1,133 @@
+{ config, pkgs, inputs, settings, lib, ... }:
+
+{
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = settings.username;
+  home.homeDirectory = "/home/${settings.username}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  nixpkgs.config.allowUnfree = true;
+  imports = [
+    inputs.illogical-impulse.homeManagerModules.default
+    inputs.spicetify-nix.homeManagerModules.default
+    (import ../../themes/stylix.nix {inherit pkgs config lib settings;})
+    ./../../user/apps/core
+    ./../../user/apps/development
+    ./../../user/apps/media
+    ./../../user/apps/emulation
+    ./../../user/apps/school
+    ./../../user/services/syncthing.nix
+    ./../../user/services/rclone
+    # ./shell.nix
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/otis/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  # BigSaltyFishes End-4 Dotfiles Setup=================
+
+
+
+  
+  illogical-impulse = {
+    # Enable Dotfiles
+    enable = true;
+    hyprland = {
+      # Monitor preference
+      monitor = settings.monitors;
+      # Use cusomize hyprland packages
+      # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      # xdgPortalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      # Set NIXOS_OZONE_WL=1
+      ozoneWayland.enable = true;
+    };
+    theme = {
+      # Customize Cursors,
+      # the following config is the default config
+      # if you don't set.
+      cursor = {
+        package = pkgs.bibata-cursors;
+        theme = "Bibata-Modern-Ice";
+      };
+    };
+    # Use custom ags package, the following package is the default.
+    # agsPackage = ags.packages.${pkgs.system}.default.override {
+    #   extraPackages = with pkgs; [ 
+    #     gtksourceview
+    #     gtksourceview4
+    #     webkitgtk
+    #     webp-pixbuf-loader
+    #     ydotool
+    #   ];
+    # };
+  };
+  wayland.windowManager.hyprland = {
+    settings = {
+      bind = [
+        "Super, L, exec, pidof hyprlock || hyprlock" # Lock
+      ];
+
+      input = {
+        kb_layout = "us";
+        kb_variant = "intl";
+      };
+
+      misc = {
+        vrr = lib.mkForce "0"; # Disable VRR
+        vfr = lib.mkForce "0"; # Disable VFR
+      };
+    };
+  };
+  
+
+
+  # ====================================================
+}

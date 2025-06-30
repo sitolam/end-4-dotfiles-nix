8c9c00316f7ba3e70050b3e5b479d09a4a1f9840
sitolam <otis.lammertyn@gmail.com>
2025-06-30 22:58:52 +0200

    cleanup settings

--- dotfiles/settings.nix
+++ dotfiles/settings.nix
@@ -21,26 +21,14 @@ rec {
         "DP-3, 1920x1080@60, 0x0, 1"
         "HDMI-A-1, 1920x1080@60, 1920x0, 1"
     ]; # Monitor preferences
-    wm = ["hyprland"]; # Selected window manager or desktop environment;
-                       # must select one in both ./user/wm/ and ./system/wm/
-                       # Note, that first WM is included into work profile
-                       # second one includes both.
 
-    font = "FiraCode Nerd Font"; # Selected font
-    fontPkg = (pkgs.nerd-fonts.fira-code);
-    fontSize = 13; # Font size
 
     icons = "Adwaita";
     iconsPkg = pkgs.adwaita-icon-theme;
     GTK_THEME = "adw-gtk3"; # GTK theme
 
     # Session variables.
-    editor = "nvim"; # Default editor
-    editorPkg = pkgs.neovim;
-    browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
-    browserPkg = pkgs.qutebrowser;
-    term = "kitty"; # Default terminal command
-    termPkg = pkgs.kitty;
+
 
     # Syncthing ID
     syncthing_ID = secrets.syncthing_ID; # Set this in secrets.nix

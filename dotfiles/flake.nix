1b218005e8911188692762025bf5c78524584823
sitolam <otis.lammertyn@gmail.com>
2025-07-01 14:38:40 +0200

    update to stable

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -1,15 +1,19 @@
 {
   description = "My Flake File";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 
     illogical-impulse.url = "github:sitolam/end-4-dots";
     illogical-impulse.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:danth/stylix";
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
 
     # Hardware Configuration
     nixos-hardware.url = "github:nixos/nixos-hardware";

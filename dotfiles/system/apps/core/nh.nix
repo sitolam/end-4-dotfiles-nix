b729ec8f7df07e4285954a9e90fcb729cd495ef3
sitolam <otis.lammertyn@gmail.com>
2025-08-10 16:25:39 +0200

    fix: add update script to upstream repo

--- dotfiles/system/apps/core/nh.nix
+++ dotfiles/system/apps/core/nh.nix
@@ -1,4 +1,18 @@
 { config, pkgs, settings, ... }:
+
+let
+  update = pkgs.writeShellScriptBin "update" ''
+    env NH_FLAKE="$NH_FLAKE"
+    nh os switch
+    if [ $? -ne 0 ]; then
+        notify-send "Error" "Failed to switch NixOS configuration."
+    else
+        notify-send "Success" "NixOS configuration switched successfully."
+    fi
+    read -n 1 -p 'Press any key to continue...'
+    '';
+in
+
 {
   programs.nh = {
     enable = true;
@@ -7,4 +21,7 @@
     flake = settings.dotfilesDir;
   };
 
+  home-manager.users.${settings.username}.home.packages = with pkgs; [
+    update
+  ];
 }

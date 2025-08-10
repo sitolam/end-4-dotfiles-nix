1789711435ddb3dd13ab83f3144ad21c9d3ec2be
sitolam <otis.lammertyn@gmail.com>
2025-08-10 17:27:00 +0200

    feat: add toggle windows script

--- dotfiles/system/virtualization/winapps/default.nix
+++ dotfiles/system/virtualization/winapps/default.nix
@@ -1,5 +1,18 @@
 { config, pkgs, settings, winapps, ...}:
 
+let
+  toggle-windows = pkgs.writeShellScriptBin "toggle-windows" ''
+    # Check the status of the kanata-default.service
+    if systemctl is-active --quiet docker-compose-winapps-root.target; then
+      systemctl stop docker-compose-winapps-root.target
+      notify-send -a 't1' -i "dialog-information" "Windows off"
+    else
+      systemctl start docker-compose-winapps-root.target
+      notify-send -a 't1' -i "dialog-information" "Windows on"
+    fi
+  '';
+in
+
 {
 
   imports = [
@@ -12,6 +25,12 @@
     winapps.packages."${settings.system}".winapps-launcher # optional
   ];
 
-  home-manager.users.${settings.username}.home.file."/home/${settings.username}/.config/winapps/winapps.conf".source = ./winapps.conf;
+  home-manager.users.${settings.username}.home = {
+    file."/home/${settings.username}/.config/winapps/winapps.conf".source = ./winapps.conf;
+
+    packages = with pkgs; [
+    toggle-windows
+    ];
+  };
 
 }
